f99b46fe95ae38f08c8ec89cd4a3946128d4c600
Cristian Mejia <crmejiam120@gmail.com>
2022-11-20 12:12:17 -0500

    corregir test 4

--- pregunta.py
+++ pregunta.py
@@ -2,7 +2,7 @@
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
 
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
@@ -22,16 +22,16 @@ def clean_data():
             df[column] = df[column].apply(str.lower)
             df[column] = df[column].apply(lambda x: x.replace("-", " "))
             df[column] = df[column].apply(lambda x: x.replace("_", " "))
-            df[column] = df[column].apply(str.strip)
-        if (column == "monto_del_credito"):
-            df[column] = df[column].apply(lambda x: x.replace(",", ""))
+            df[column] = df[column].apply(lambda x: x.replace("$ ", ""))
             df[column] = df[column].apply(lambda x: x.replace("$", ""))
+            df[column] = df[column].apply(lambda x: x.replace(",", ""))
             df[column] = df[column].apply(lambda x: x.replace(".00", ""))
+        if (column == "monto_del_credito"):
             df[column] = df[column].apply(lambda x: int(x))
         if (column == "comuna_ciudadano"):
             df[column] = df[column].apply(lambda x: float(x))
         if (column == "fecha_de_beneficio"):
-            df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/",x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+            df[column] = df[column].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/",x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
     df = df.drop_duplicates().reset_index(drop=True)
 
     return df
